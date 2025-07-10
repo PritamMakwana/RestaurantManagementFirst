2e3df0788bdc43a751eb96ff46dae3ea4aa59337
Pritam Makwana <pritammakwana17561@gmail.com>
2025-07-10 10:37:17 +0530

    Complete Sign Page and Redirections

---
+++ src/components/HomePage.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>Hello,welcome On HomePage page</h1>
+</template>
+<script>
+export default {
+    name: "HomePage",
+};
+</script>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -3,15 +3,44 @@
     <img src="../assets/restaurant-logo.jpg" alt="Logo" class="logo" />
     <h1>Sign Up</h1>
     <div class="register">
-        <input type="text" placeholder="Enter name" />
-        <input type="text" placeholder="Enter email" />
-        <input type="password" placeholder="Enter Password" />
-        <button>Sign Up</button>
+        <input type="text" v-model="name" placeholder="Enter name" />
+        <input type="text" v-model="email" placeholder="Enter email" />
+        <input type="password" v-model="password" placeholder="Enter Password" />
+        <button v-on:click="signUp">Sign Up</button>
     </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
     name: "SignUp",
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: '',
+        }
+    },
+    methods: {
+        async signUp() {
+            console.log("Sign Up clicked", this.name, this.email, this.password);
+            let result = await axios.post("http://localhost:3000/user", {
+                name: this.name,
+                email: this.email,
+                password: this.password
+            });
+            console.log("Result from server", result);
+            if (result.status === 201) {
+                // alert("User registered successfully");
+                localStorage.setItem("user-info", JSON.stringify(result.data));
+                this.$router.push({ name: 'HomePage' });
+            } else {
+                alert("Error in registration");
+            }
+
+
+
+        }
+    }
 
 }
 </script>
@@ -20,7 +49,7 @@ export default {
     width: 100px;
 }
 
-.register input{
+.register input {
     width: 300px;
     height: 40px;
     padding-left: 20px;
