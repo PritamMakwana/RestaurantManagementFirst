df6c2f215a5bcedf08538db9e65ee2d83085e2fa
Pritam Makwana <pritamcodehanger22@gmail.com>
2025-07-12 16:46:31 +0530

    update and delete

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -7,12 +7,16 @@
             <th>Name</th>
             <th>Aaddress</th>
             <th>Contact</th>
+            <th>Action</th>
         </tr>
         <tr v-for="item in restaurants" :key="item.id">
             <td>{{ item.id }}</td>
             <td>{{ item.name }}</td>
             <td>{{ item.address }}</td>
             <td>{{ item.contact }}</td>
+            <td><router-link :to="'/update/' + item.id">Update</router-link>
+                <button v-on:click="deleteRestarent(item.id)">delete</button>
+            </td>
         </tr>
     </table>
 </template>
@@ -30,14 +34,27 @@ export default {
     components: {
         HeaderLayout
     },
-    async mounted() {
-        let user = localStorage.getItem('user-info');
-        this.name = JSON.parse(user).name
-        if (!user) {
-            this.$router.push({ name: "SignUp" });
+    methods: {
+        async deleteRestarent(id) {
+            console.log('delete' + id);
+            let result = await axios.delete("http://localhost:3000/restaurant/" + id);
+            if (result.status == 200) {
+                console.log('delete');
+                this.loadData();
+            }
+        },
+        async loadData() {
+            let user = localStorage.getItem('user-info');
+            this.name = JSON.parse(user).name
+            if (!user) {
+                this.$router.push({ name: "SignUp" });
+            }
+            let result = await axios.get("http://localhost:3000/restaurant");
+            this.restaurants = result.data;
         }
-        let result = await axios.get("http://localhost:3000/restaurant");
-        this.restaurants = result.data;
+    },
+    async mounted() {
+        this.loadData();
     }
 };
 </script>
--- src/components/UpdatePage.vue
+++ src/components/UpdatePage.vue
@@ -1,19 +1,60 @@
 <template>
-      <HeaderLayout />
+    <HeaderLayout />
     <h1>Hello,welcome On UpdatePage page</h1>
+    <form class="add">
+        <input type="text" placeholder="Enter Name" v-model="restaurent.name" name="name" />
+        <input type="text" placeholder="Enter Address" v-model="restaurent.address" name="address" />
+        <input type="text" placeholder="Enter Contact" v-model="restaurent.contact" name="contact" />
+        <button type="button" v-on:click="updateRestaurent">Update Restaurent</button>
+    </form>
 </template>
 <script>
+import axios from 'axios';
 import HeaderLayout from './HeaderLayout.vue'
 export default {
     name: "UpdatePage",
     components: {
         HeaderLayout
     },
-    mounted() {
+    data() {
+        return {
+            restaurent: {
+                name: '',
+                address: '',
+                contact: ''
+            }
+
+        }
+    },
+    methods: {
+        async updateRestaurent() {
+            try {
+                console.log("Trying to update ID:", this.$route.params.id);
+                const result = await axios.put("http://localhost:3000/restaurant/" + this.$route.params.id, {
+                    name: this.restaurent.name,
+                    address: this.restaurent.address,
+                    contact: this.restaurent.contact
+                });
+
+                if (result.status == 200) {
+                    this.$router.push({ name: 'HomePage' });
+                }
+            } catch (error) {
+                console.error("Update failed:", error);
+                alert("Update failed. Please check if the ID exists and server is running.");
+            }
+        }
+    },
+    async mounted() {
+
         let user = localStorage.getItem('user-info');
         if (!user) {
             this.$router.push({ name: "SignUp" });
         }
+
+        const result = await axios.get('http://localhost:3000/restaurant?id=' + this.$route.params.id);
+
+        this.restaurent = result.data[0];
     }
 };
 </script>
